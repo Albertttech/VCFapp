9edc71f05e38acf2f7df330643d3a88cc5497980
Albertttech <Alberttechpush@gmail.com>
2025-07-07 15:33:57 +0100

    Update settings.py

--- vcfproject/vcfproject/settings.py
+++ vcfproject/vcfproject/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -115,10 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "vcfviewer" / "static",
-]
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Static files config
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "vcfviewer" / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
