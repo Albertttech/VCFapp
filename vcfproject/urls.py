415025c71a0d7dc08437c3567e723581034816c0
Gideon <Alberttechpush@gmail.com>
2025-07-22 14:44:27 +0100

    Initial commit

---
+++ vcfproject/urls.py
@@ -0,0 +1,9 @@
+# vcfproject/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('vcfviewer.urls')),  # Include our app's URLs
+]
---
+++ vcfviewer/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class VCFFile(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    max_contacts = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+  
+class Contact(models.Model):
+    vcf_file = models.ForeignKey(VCFFile, on_delete=models.CASCADE, related_name='contacts')
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20)
+
+    class Meta:
+        unique_together = ('vcf_file', 'phone')
+
+    def __str__(self):
+        return f"{self.name} ({self.phone})"
